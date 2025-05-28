a3a5886f5deb3f5e9f1a842cc6f50f11fd4929f5
hanumanta97 <hanumantamannade1997@gmail.com>
2025-05-28 20:17:35 +0530

    Update kafka_producer.py

--- Backend/kafka_producer.py
+++ Backend/kafka_producer.py
@@ -1,16 +1,15 @@
 from kafka import KafkaProducer
-import time
 
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
-    value_serializer=lambda v: v  # no serialization
+    acks='all',
+    linger_ms=10,
+    batch_size=32 * 1024,
+    value_serializer=lambda v: v
 )
 
 def send_video_chunks(video_data: bytes, chunk_size=512 * 1024):
     for i in range(0, len(video_data), chunk_size):
         chunk = video_data[i:i + chunk_size]
-        # print(f"Sending chunk {i//chunk_size}")
         producer.send('video-stream', chunk)
-        time.sleep(0.1)  # Let browser buffer
     producer.flush()
-
