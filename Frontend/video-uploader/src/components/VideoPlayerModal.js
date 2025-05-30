13a3ea603de00599ede3b10edd4894eef97eca27
hanumanta97 <hanumantamannade1997@gmail.com>
2025-05-30 10:56:22 +0530

    Update VideoPlayerModal.js

--- Frontend/video-uploader/src/components/VideoPlayerModal.js
+++ Frontend/video-uploader/src/components/VideoPlayerModal.js
@@ -1,16 +1,4 @@
-import React from "react";
-import { Modal } from "react-bootstrap";
-
-const VideoPlayerModal = ({ show, onHide, videoId }) => {
-    console.log(`${process.env.REACT_APP_API_URL}/video/${videoId}`);
-
-  return (
-    <Modal show={show} onHide={onHide} size="lg" centered>
-      <Modal.Header closeButton>
-        <Modal.Title>🎥 Playing Video</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {videoId && (import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Modal } from "react-bootstrap";
 
 const VideoPlayerModal = ({ show, onHide, videoId }) => {
@@ -49,18 +37,6 @@ const VideoPlayerModal = ({ show, onHide, videoId }) => {
         {videoUrl && (
           <video controls width="100%" autoPlay>
             <source src={videoUrl} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        )}
-      </Modal.Body>
-    </Modal>
-  );
-};
-
-export default VideoPlayerModal;
-
-          <video controls width="100%" autoPlay>
-            <source src={`${process.env.REACT_APP_API_URL}/video/${videoId}`} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         )}
